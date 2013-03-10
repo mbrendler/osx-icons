4a4627af789afd62d5bdf8c3219aa20931477c22
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-03-10 19:38:37 +0100

    Add informations and warnings.

--- create_iconset.sh
+++ create_iconset.sh
@@ -26,6 +26,9 @@ if test ! -e "$IN_IMAGE" ; then
   exit
 fi
 
+function info () { echo -e "\033[38;5;10m$1\033[0m"; }
+function warn () { echo -e "\033[38;5;11m$1\033[0m"; }
+
 function add_image_with_size() {
   local SIZE=$1
   if test "$2" != "2" ; then
@@ -35,7 +38,7 @@ function add_image_with_size() {
     local SIZE_FILENAME=icon_"$SIZE"x"$SIZE"@2x.png
     local REAL_SIZE=$(($SIZE * 2))
   fi
-  echo -e "\033[38;5;10mcreate icon $SIZE_FILENAME\033[0m"
+  info "create icon $SIZE_FILENAME"
   sips -Z $REAL_SIZE --out "$ICON_SET"/"$SIZE_FILENAME" "$IN_IMAGE"
 }
 
@@ -48,3 +51,20 @@ for SIZE in 16 32 128 256 512 ; do
   add_image_with_size $SIZE
   add_image_with_size $SIZE "2"
 done
+
+HEIGHT=$(sips -g pixelHeight $IN_IMAGE | grep pixelHeight | cut -d\  -f4)
+WIDTH=$(sips -g pixelWidth $IN_IMAGE | grep pixelWidth | cut -d\  -f4)
+if test "$HEIGHT" != "$WIDTH" ||
+   test "$HEIGHT" -lt "1024" ||
+   test "$WIDTH" -lt "1024" ; then
+  echo $HEIGHT x $WIDTH
+  warn "WARNING: The input image should be bigger than 1024x1024 and a quad."
+  warn "         But is $HEIGHT"x"$WIDTH".
+fi
+
+IN_IMAGE_TYPE=$(sips -g format $IN_IMAGE | grep format | cut -d\  -f4)
+if test "$IN_IMAGE_TYPE" != "png" ; then
+  warn "WARNING: The input image should be a png but is a $IN_IMAGE_TYPE."
+fi
+
+info "Icon set created: $ICON_SET"
