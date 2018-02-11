233eca9d16c1f5674bf547d3e1a242ebaee7ee5a
Maik Brendler <maik.brendler@injixo.com>
2018-02-11 14:55:43 +0100

    Add iPhone support

--- create_iconset.sh
+++ create_iconset.sh
@@ -7,13 +7,20 @@
 
 set -e
 
+TARGET=macos
+
+if [ "$1" = '--iphone' ]; then
+  TARGET=iphone
+  shift
+fi
+
 ICON_NAME=$1
 ICON_SET="$ICON_NAME".iconset
 
 IN_IMAGE=$2
 
 function usage () {
-  echo "usage: $0 ICON_NAME IN_IMAGE"
+  echo "usage: $0 [--iphone] ICON_NAME IN_IMAGE"
   exit
 }
 
@@ -31,12 +38,13 @@ function warn () { echo -e "\033[38;5;11m$1\033[0m"; }
 
 function add_image_with_size() {
   local SIZE=$1
-  if test "$2" != "2" ; then
+  if test "$2" = "" ; then
     local SIZE_FILENAME=icon_"$SIZE"x"$SIZE".png
     local REAL_SIZE=$SIZE
   else
-    local SIZE_FILENAME=icon_"$SIZE"x"$SIZE"@2x.png
-    local REAL_SIZE=$((SIZE * 2))
+    local factor="$2"
+    local SIZE_FILENAME=icon_"$SIZE"x"$SIZE"@"$factor"x.png
+    local REAL_SIZE=$((SIZE * factor))
   fi
   info "create icon $SIZE_FILENAME"
   sips -Z $REAL_SIZE --out "$ICON_SET"/"$SIZE_FILENAME" "$IN_IMAGE"
@@ -44,13 +52,21 @@ function add_image_with_size() {
 
 mkdir "$ICON_SET"
 
-# needed sizes: 16, 32, 128, 256, 512 pixels
-# names: icon_16x16.png, ...
-# retina support needs also icons with double resolution: icon_16x16@2x.png, ...
-for SIZE in 16 32 128 256 512 ; do
-  add_image_with_size $SIZE
-  add_image_with_size $SIZE "2"
-done
+if [ "$TARGET" = iphone ] ; then
+  for SIZE in 20 29 40 60 ; do
+    add_image_with_size $SIZE 2
+    add_image_with_size $SIZE 3
+  done
+  add_image_with_size 1024
+else
+  # needed sizes: 16, 32, 128, 256, 512 pixels
+  # names: icon_16x16.png, ...
+  # retina support needs also icons with double resolution: icon_16x16@2x.png, ...
+  for SIZE in 16 32 128 256 512 ; do
+    add_image_with_size $SIZE
+    add_image_with_size $SIZE 2
+  done
+fi
 
 HEIGHT="$(sips -g pixelHeight "$IN_IMAGE" | grep pixelHeight | cut -d\  -f4)"
 WIDTH="$(sips -g pixelWidth "$IN_IMAGE" | grep pixelWidth | cut -d\  -f4)"
