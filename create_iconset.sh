1637e75e269e50a5178b6af38f7b9aeed10cb5f4
Maik Brendler <maik.brendler@injixo.com>
2018-02-11 14:35:59 +0100

    Fix warnings

--- create_iconset.sh
+++ create_iconset.sh
@@ -13,7 +13,7 @@ ICON_SET="$ICON_NAME".iconset
 IN_IMAGE=$2
 
 function usage () {
-  echo usage: $0 ICON_NAME IN_IMAGE
+  echo "usage: $0 ICON_NAME IN_IMAGE"
   exit
 }
 
@@ -22,7 +22,7 @@ if test "$ICON_NAME" == "" || test "$IN_IMAGE" == ""; then
 fi
 
 if test ! -e "$IN_IMAGE" ; then
-  echo Input image $IN_IMAGE does not exist.
+  echo "Input image $IN_IMAGE does not exist."
   exit
 fi
 
@@ -36,13 +36,13 @@ function add_image_with_size() {
     local REAL_SIZE=$SIZE
   else
     local SIZE_FILENAME=icon_"$SIZE"x"$SIZE"@2x.png
-    local REAL_SIZE=$(($SIZE * 2))
+    local REAL_SIZE=$((SIZE * 2))
   fi
   info "create icon $SIZE_FILENAME"
   sips -Z $REAL_SIZE --out "$ICON_SET"/"$SIZE_FILENAME" "$IN_IMAGE"
 }
 
-mkdir $ICON_SET
+mkdir "$ICON_SET"
 
 # needed sizes: 16, 32, 128, 256, 512 pixels
 # names: icon_16x16.png, ...
@@ -52,17 +52,17 @@ for SIZE in 16 32 128 256 512 ; do
   add_image_with_size $SIZE "2"
 done
 
-HEIGHT=$(sips -g pixelHeight $IN_IMAGE | grep pixelHeight | cut -d\  -f4)
-WIDTH=$(sips -g pixelWidth $IN_IMAGE | grep pixelWidth | cut -d\  -f4)
+HEIGHT="$(sips -g pixelHeight "$IN_IMAGE" | grep pixelHeight | cut -d\  -f4)"
+WIDTH="$(sips -g pixelWidth "$IN_IMAGE" | grep pixelWidth | cut -d\  -f4)"
 if test "$HEIGHT" != "$WIDTH" ||
    test "$HEIGHT" -lt "1024" ||
    test "$WIDTH" -lt "1024" ; then
-  echo $HEIGHT x $WIDTH
+  echo "$HEIGHT x $WIDTH"
   warn "WARNING: The input image should be bigger than 1024x1024 and a quad."
-  warn "         But is $HEIGHT"x"$WIDTH".
+  warn "         But is $HEIGHT x $WIDTH".
 fi
 
-IN_IMAGE_TYPE=$(sips -g format $IN_IMAGE | grep format | cut -d\  -f4)
+IN_IMAGE_TYPE="$(sips -g format "$IN_IMAGE" | grep format | cut -d\  -f4)"
 if test "$IN_IMAGE_TYPE" != "png" ; then
   warn "WARNING: The input image should be a png but is a $IN_IMAGE_TYPE."
 fi
